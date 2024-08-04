f9befdcf9c9f5781cefb6d9b86df26780612d7ea
Mohit Lokhande <55676553+Vaibhav-13@users.noreply.github.com>
2024-08-04 09:52:28 +0530

    Update Add.js

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -4,15 +4,15 @@ function Add() {
   const openPopup = () => {
     // Open the popup window
     window.open(
-      'http://localhost:3000/popup-form',
+      'http://localhost:3000/popup-form', //to load form
       'popupForm',
-      'width=500,height=300'
+      'width=500,height=400' //hight and width
     );
   };
 
   return (
     <>
-      <button id="button" onClick={openPopup}> 
+      <button id="button" onClick={openPopup}> {/*on user click open form window*/}
         Add Data
       </button>
     </>
