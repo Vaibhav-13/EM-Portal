4e81c5a0f71e60ab5668c585fd123a8b7dceef10
Mohit Lokhande <55676553+Vaibhav-13@users.noreply.github.com>
2024-08-04 09:56:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,32 +10,36 @@ app.use(express.json());
 
 // Define the schema with correct types
 const DataSchema = new mongoose.Schema({
-  id: { type: Number, required: true },
+  empid: { type: Number, required: true },
   name: { type: String, required: true },
-  gender: { type: String, default: '' },
+  gender: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: String, required: true }, 
-  city: { type: String, default: '' }, 
+  phone: { type: String, required: true },
+  city: { type: String, required: true },
   dob: { type: String, required: true },
   careerlevel: { type: Number, required: true }
 });
 
+
+
 const Data = mongoose.model('Employee_Info', DataSchema);
 
+//DB connection
 mongoose.connect('mongodb://127.0.0.1:27017/EmployeeDB')
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
+//to post or add data in DB
 app.post('/api/data', async (req, res) => {
-  const { id, name, gender, email, phone, city, dob, careerlevel } = req.body;
+  const { empid, name, gender, email, phone, city, dob, careerlevel } = req.body;
 
   // Validate the incoming data
-  if (!id || !name || !email || !phone || !dob || !careerlevel) {
+  if (!empid || !name || !email || !phone || !dob || !careerlevel) {
     return res.status(400).json({ message: 'All required fields must be provided' });
   }
 
   const newData = new Data({
-    id,
+    empid,
     name,
     gender,
     email,
@@ -54,6 +58,7 @@ app.post('/api/data', async (req, res) => {
   }
 });
 
+//to get or fetch data from DB
 app.get('/api/data', async (req, res) => {
   try {
     const employees = await Data.find();
@@ -63,6 +68,36 @@ app.get('/api/data', async (req, res) => {
   }
 });
 
+//to update data in DB
+app.put('/api/data/:empid', async (req, res) => {
+  const { empid } = req.params;
+  const updatedData = req.body;
+
+  try {
+    const updatedEmployee = await Data.findOneAndUpdate({ empid: empid }, updatedData, { new: true });
+    if (!updatedEmployee) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+    res.json({ message: 'Employee updated successfully', data: updatedEmployee });
+  } catch (err) {
+    res.status(500).json('Error: ' + err);
+  }
+});
+
+//to delete data from DB
+app.delete('/api/data/:empid', async (req, res) => {
+  try {
+    const deletedItem = await Data.findOneAndDelete({ empid: req.params.empid });
+    if (!deletedItem) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    res.json({ message: 'Item deleted successfully' });
+  } catch (err) {
+    res.status(500).json('Error: ' + err);
+  }
+});
+
+//to check server is up and running
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
