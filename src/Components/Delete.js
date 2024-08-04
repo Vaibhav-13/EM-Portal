38f28f3e4f967f4dc1e323ce51e3d525f8a9cda9
Mohit Lokhande <55676553+Vaibhav-13@users.noreply.github.com>
2024-08-04 09:52:47 +0530

    Update Delete.js

--- src/Components/Delete.js
+++ src/Components/Delete.js
@@ -1,10 +1,22 @@
-import "../Stylesheets/Component.css"
+import React from 'react';
+import axios from 'axios';
+import "../Stylesheets/Component.css";
 
-function Delete(){
-    return(
-        <>
-        <button id="button">Delete Data</button>
-        </>
-    )
+function Delete({ empid, onDelete }) { //to get the empid and onDelete 
+  const handleDelete = async () => { 
+    try {
+      await axios.delete(`http://localhost:5000/api/data/${empid}`);//connect to db and get empid
+      onDelete(empid); //call onDelete method
+      alert('Data deleted successfully');
+    } catch (error) {
+      console.error('Error deleting data:', error);
+      alert('Failed to delete data');
+    }
+  };
+
+  return (
+    <button id='button' onClick={handleDelete}>Delete</button> //onClick go to handleDelete function
+  );
 }
+
 export default Delete;
