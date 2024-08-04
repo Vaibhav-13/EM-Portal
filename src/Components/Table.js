0579a7cdc469d2fe947e9703c1b41fd7108c1d39
Mohit Lokhande <55676553+Vaibhav-13@users.noreply.github.com>
2024-08-04 09:54:15 +0530

    Update Table.js

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,100 +1,89 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from 'axios'; //used to send asynchronous HTTP requests to REST endpoints
+import Delete from './Delete';
+import Update from './Update';
 import "../Stylesheets/Table.css";
-/* Hard coded Data for table
-  const data = [    
-    {
-        id: 87873,
-        name: "Mahi",
-        gender: "FeMale",
-        email: "yuas@gmail.com",
-        phone: 1234567890,
-        city: "Mumbai",
-        dob: "19/11/1998",
-        clevel: 10,
-    },
-    {
-        id: 12345,
-        name: "Ram",
-        gender: "Male",
-        email: "azxd@gmail.com",
-        phone: 7855567890,
-        city: "Pune",
-        dob: "13/11/2001",
-        clevel: 11,
-    },{
-        id: 585,
-        name: "Lisa",
-        gender: "FeMale",
-        email: "zkw@gmail.com",
-        phone: 1555457890,
-        city: "Mumbai",
-        dob: "19/01/1996",
-        clevel: 9,
-    },
-    {
-        id: 5168,
-        name: "John",
-        gender: "Male",
-        email: "ased@gmail.com",
-        phone: 8934567890,
-        city: "Pune",
-        dob: "13/08/2010",
-        clevel: 13,
-    },
-  ];
-  */
-  function Table() {
-    
-      const [data, setData] = useState([]);
+import { FaSearch } from "react-icons/fa";
 
+function Table() {
+  const [data, setData] = useState([]); //useState is used for storing variables
+  const [selectedEmpId, setSelectedEmpId] = useState([]);
+  
   useEffect(() => {
-    // Fetch data from the backend
-    axios.get('http://localhost:5000/api/data')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+    fetchData(); // useEffect is used here for re-rendering the data
   }, []);
-  return (
-      <div className="App">
-       
-        <table className="container">
-          <thead>
-            <tr>
-                <th>Employee ID</th>
-                <th>Name</th>
-                <th>Gender</th>
-                <th>Email</th>
-                <th>Phone Number</th>
-                <th>City</th>
-                <th>DOB</th>
-                <th>Career Level</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((value, key) => {
-              return (
-                <tr key={key}>
-                  <td>{value.id}</td>
-                  <td>{value.name}</td>
-                  <td>{value.gender}</td>
-                  <td>{value.email}</td>
-                  <td>{value.phone}</td>
-                  <td>{value.city}</td>
-                  <td>{value.dob}</td>
-                  <td>{value.careerlevel}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
 
-  
-  
-  export default Table;
+  const fetchData = async () => { //fetch data
+    try {
+      const response = await axios.get('http://localhost:5000/api/data');
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleDelete = (empid) => { //to delete data for empid
+    setData(data.filter(item => item.empid !== empid)); //data will removed
+  };
+
+
+  const handleSearch = (empid) => { //to update data
+     //fetch data
+  };
+  const handleUpdate = (empid) => { //to update data
+    fetchData(); //fetch data
+    setSelectedEmpId(empid); //to update data for empid
+  };
+
+
+
+  return (
+    <div className="App">
+      <div id="search">
+        <input placeholder='Enter Employee ID' ></input> 
+        <button id='icon' onClick={handleSearch}>
+             <FaSearch/>
+        </button> 
+      </div>
+      <table className="container"> {/*Print database data in table format*/}
+        <thead>
+          <tr>
+            <th>Employee ID</th>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>City</th>
+            <th>DOB</th>
+            <th>Career Level</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((value) => (
+            <tr key={value.empid}>
+              <td>{value.empid}</td>
+              <td>{value.name}</td>
+              <td>{value.gender}</td>
+              <td>{value.email}</td>
+              <td>{value.phone}</td>
+              <td>{value.city}</td>
+              <td>{value.dob}</td>
+              <td>{value.careerlevel}</td>
+              <td>
+                <Delete empid={value.empid} onDelete={handleDelete} /> {/*to delete data for empid */}
+               {selectedEmpId === value.empid ? (
+                  <Update empid={value.empid} onUpdate={handleUpdate} /> //to update data for empid
+                ) : (
+                  <button id="button" onClick={() => setSelectedEmpId(value.empid)}>Update</button> 
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
